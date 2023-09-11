0f21429b8df1e893d7da87fd2f1f6950687707a4
Mauricio Carrasco Núñez <mau.carrasco@uc.cl>
2023-09-11 10:30:18 -0300

    actualización de códigos para la creación de la base de datos de víctimas

--- victimas/codigos/base_victimas.R
+++ victimas/codigos/base_victimas.R
@@ -39,4 +39,21 @@ victimas_pdh <- victimas_pdh %>%
          sexo = factor(sexo),
          tipo_actividad = factor(tipo_actividad))
 
+#### Unión de bases de datos ####
 
+victimas <- victimas_pdh %>%
+  left_join(victimas_museo %>%
+              select(id, fecha_desaparicion_muerte, region, ciudad, comuna, comision, calificacion, relato)) %>%
+  left_join(victimas_df %>%
+              select(id, tipo_caso = TIPO, identificacion = IDENTIFICACIÓN_REC, identificacion_det = IDENTIFICACIÓN)) %>%
+  mutate(tipo_caso = case_when(tipo_caso %in% c("DETENIDA DESAPARECIDA", "DETENIDO DESAPARECIDO") ~ 1,
+                               tipo_caso %in% c("EJECUTADA POLITICA S/E CUERPO", "EJECUTADO POLITICO S/E CUERPO") ~ 2,
+                               TRUE ~ 3),
+         tipo_caso = factor(tipo_caso, labels = c("DESAPARECIDO/A", "ASESINADO/A SIN ENTREGA DE CUERPO", "ASESINADO/A")))
+
+victimas <- victimas %>%
+  select(1:17, 19:21, 18)
+
+#### Guardado de base de datos ####
+
+openxlsx::write.xlsx(victimas, "C:/Users/mauricio.carrasco/Desktop/PNB/victimas/resultados/victimas.xlsx")
